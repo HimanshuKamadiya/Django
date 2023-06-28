362fe98f8b03551c9f51a5227f130342f31ffd94
Himanshu Kamadiya <123982410+HimanshuKamadiya@users.noreply.github.com>
2023-06-28 16:29:49 +0530

    new

---
+++ site1/site1/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return  render(request, 'index.html')
+
+def sum(request):
+    return HttpResponse('html')
+
+def dynamic_data(request):
+    data={
+        'name': ['django','web-dev','class'],
+        }
+    return render(request,'data.html',data)
+
+def form(request):
+    return render(request,'dynamic.html')
