dd622a854b7aadaccfc401618fadf580fcb908e0
Himanshu Kamadiya <123982410+HimanshuKamadiya@users.noreply.github.com>
2023-06-30 16:16:29 +0530

    new

--- site1/site1/views.py
+++ site1/site1/views.py
@@ -14,4 +14,22 @@ def dynamic_data(request):
     return render(request,'data.html',data)
 
 def form(request):
-    return render(request,'dynamic.html')
+    ans=0
+    try:
+        n=int(request.GET['num'])
+        n1=int(request.GET['num1'])
+        n2=request.GET['operator']
+        if n2 =='+':
+            ans=n+n1
+        elif n2=='-':
+            ans=n-n1
+        elif n2=='/':
+            ans=n%n1
+        elif n2=='x':
+            ans=n*n1
+        else:
+            ans='error'
+    except:
+        pass
+    return render(request,'form.html',{'out':ans})
+        
