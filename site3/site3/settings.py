fd99019cafe02e65ed4f057d68be08a1992e5112
himanshukamadiya <himanshukamadiya@yahoo.com>
2023-07-24 01:03:28 +0530

    new

--- site3/site3/settings.py
+++ site3/site3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!rvpdil%@u0=w88p)t-*z#=$rxv(o6w@gil!bpz8o3jhec1t2g'
+SECRET_KEY = 'django-insecure-6c21ad*x4jw*gsj&(xs(ygq$)1#rs!&yxn0yq_4lg65t+!n%t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'poll'
 ]
 
 MIDDLEWARE = [
@@ -122,9 +121,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
