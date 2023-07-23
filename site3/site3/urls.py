22265efc152486b880df9ebd2ba2b9e126638453
himanshukamadiya <himanshukamadiya@yahoo.com>
2023-07-24 01:03:21 +0530

    new

--- site3/site3/urls.py
+++ site3/site3/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib import  admin
-from site3 import views
+from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name= 'index'),
-    
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
