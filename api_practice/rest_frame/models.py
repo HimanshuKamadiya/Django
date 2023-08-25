3f543b1062a506b4385d20482ad53ebd25efa242
himanshukamadiya <himanshukamadiya@yahoo.com>
2023-08-25 09:30:02 +0530

    new

--- api_practice/rest_frame/models.py
+++ api_practice/rest_frame/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class rest_api_function(models.Model):
     rest_model_1=models.CharField(max_length=100)
-    rest_model_2=models.TextField()
+    rest_model_2=models.IntegerField()
     
     def __str__(self):
       return self.rest_model_1
