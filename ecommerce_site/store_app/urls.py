ce49a8621d1db7cadc8c56b1a49ea13cb8776008
Himanshu Kamadiya <123982410+HimanshuKamadiya@users.noreply.github.com>
2024-02-20 23:54:12 +0530

    Delete ecommerce_site directory

--- ecommerce_site/store_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('',views.e_store,name="store"),
-    path('cart/',views.cart,name='cart'),
-    path('checkout/',views.checkout,name='checkout')
-]
